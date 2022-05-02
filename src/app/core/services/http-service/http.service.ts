c90fa987d55ef68730469fedc845cdfce8417742
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2022-05-02 21:08:35 +0300

    feat: add error handler for http-service

--- src/app/core/services/http-service/http.service.ts
+++ src/app/core/services/http-service/http.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { catchError, Observable } from "rxjs";
 import { environment } from "@environments/environment";
 
 @Injectable({
@@ -10,22 +10,47 @@ export class HttpService {
   constructor(private http: HttpClient) {}
 
   get<T>(chunk: string): Observable<T> {
-    return this.http.get<T>(`${environment.API_URL}/${chunk}`);
+    return this.http.get<T>(`${environment.API_URL}/${chunk}`).pipe(
+      catchError((err, caught) => {
+        console.error(err);
+        return caught;
+      }),
+    );
   }
 
   getAll<T>(chunk: string): Observable<T[]> {
-    return this.http.get<T[]>(`${environment.API_URL}/${chunk}`);
+    return this.http.get<T[]>(`${environment.API_URL}/${chunk}`).pipe(
+      catchError((err, caught) => {
+        console.error(err);
+        return caught;
+      }),
+    );
   }
 
   post<T>(chunk: string, body: T): Observable<T> {
-    return this.http.post<T>(`${environment.API_URL}/${chunk}`, body);
+    return this.http.post<T>(`${environment.API_URL}/${chunk}`, body).pipe(
+      catchError((err, caught) => {
+        console.error(err);
+        return caught;
+      }),
+    );
   }
 
   update<T>(chunk: string, body: T): Observable<T> {
-    return this.http.put<T>(`${environment.API_URL}/${chunk}`, body);
+    return this.http.put<T>(`${environment.API_URL}/${chunk}`, body).pipe(
+      catchError((err, caught) => {
+        console.error(err);
+        return caught;
+      }),
+    );
   }
 
   delete<T>(chunk: string): Observable<T> {
-    return this.http.delete<T>(`${environment.API_URL}/${chunk}`);
+    return this.http.delete<T>(`${environment.API_URL}/${chunk}`).pipe(
+      catchError((err, caught) => {
+        console.error(err);
+        return caught;
+      }),
+    );
   }
 }
