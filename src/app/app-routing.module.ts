d1830738faad907a4332e1a7d51214c744376482
Maksim <honestliar91@gmail.com>
2022-05-04 14:20:44 +0300

    feat: add not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HOME_PAGE } from "@utils";
+import { NotFoundComponent } from "@app/core/pages/not-found/not-found.component";
 
 const routes: Routes = [
   { path: "", redirectTo: `/${HOME_PAGE}`, pathMatch: "full" },
   { path: "home", loadChildren: () => import("./home/home.module").then((m) => m.HomeModule) },
+  { path: "**", component: NotFoundComponent },
 ];
 
 @NgModule({
