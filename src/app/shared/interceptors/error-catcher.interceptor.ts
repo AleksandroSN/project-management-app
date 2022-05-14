b8e2c9fb80949f05dfbe4b3e4ba924c7486a4c2e
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2022-05-14 20:11:23 +0300

    fix: exclude boardId route from error catcher

--- src/app/shared/interceptors/error-catcher.interceptor.ts
+++ src/app/shared/interceptors/error-catcher.interceptor.ts
@@ -7,6 +7,7 @@ import {
 } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { NotificationsService } from "@app/core/services";
+import { BOARDS_ENDPOINT, checkLenghtStr } from "@utils";
 import { catchError, Observable, throwError } from "rxjs";
 import { BackendError } from "../models";
 
@@ -15,6 +16,10 @@ export class ErrorCatcherInterceptor implements HttpInterceptor {
   constructor(private notificationService: NotificationsService) {}
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const isBoardWithIdRoute = checkLenghtStr(BOARDS_ENDPOINT, req.url);
+    if (req.url.includes(BOARDS_ENDPOINT) && !isBoardWithIdRoute) {
+      return next.handle(req);
+    }
     return next.handle(req).pipe(
       catchError((errorResponse: HttpErrorResponse) => {
         const { message, statusCode } = errorResponse.error as BackendError;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./constants";
 export * from "./checkExpTimeToken";
+export * from "./checkLengthStr";
