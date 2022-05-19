a1d8b856b4119632b5bd016316edd2bddb031aec
Mironov Vladimir <zidiks228@gmail.com>
2022-05-19 12:45:30 +0300

    upd: add back btn on detail board page

--- src/app/shared/components/header-to-component/header-to-components.component.ts
+++ src/app/shared/components/header-to-component/header-to-components.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-header-to-component",
@@ -7,4 +8,14 @@ import { Component, Input } from "@angular/core";
 })
 export class HeaderToComponent {
   @Input() public title!: string;
+
+  public boardPath: boolean = false;
+
+  constructor(public route: ActivatedRoute, public router: Router) {
+    this.boardPath = window.location.href.includes("/board/");
+  }
+
+  public back(): void {
+    this.router.navigateByUrl("/board");
+  }
 }
